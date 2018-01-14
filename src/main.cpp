30bc18dd21f9752276e7dcf1f1459c3edd8cd23d
Yangchun Luo <timycluo@gmail.com>
2018-01-13 17:16:46 -0800

    implement calculateRMSE

--- src/main.cpp
+++ src/main.cpp
@@ -104,31 +104,25 @@ int main() {
     // Call ProcessMeasurment(meas_package) for Kalman filter
     fusionEKF.ProcessMeasurement(meas_package);
 
-    // Push the current estimated x,y positon from the Kalman filter's state vector
-    VectorXd estimate(4);
-    double p_x = fusionEKF.ekf_.x_(0);
-    double p_y = fusionEKF.ekf_.x_(1);
-    double v1  = fusionEKF.ekf_.x_(2);
-    double v2 = fusionEKF.ekf_.x_(3);
-    estimate(0) = p_x;
-    estimate(1) = p_y;
-    estimate(2) = v1;
-    estimate(3) = v2;
+    // Make a copy of the current estimation from the Kalman filter's state vector
+    VectorXd estimate = fusionEKF.ekf_.x_;
+    assert(&estimate != &fusionEKF.ekf_.x_);
     estimations.push_back(estimate);
 
-    //VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
+    // Compute the running error metric
+    VectorXd RMSE = tools.CalculateRMSE(estimations, ground_truth);
 
+    // Assemble the message to be sent back
     json msgJson;
-    msgJson["estimate_x"] = p_x;
-    msgJson["estimate_y"] = p_y;
-    msgJson["rmse_x"] =  0;
-    msgJson["rmse_y"] =  0;
-    msgJson["rmse_vx"] = 0;
-    msgJson["rmse_vy"] = 0;
+    msgJson["estimate_x"] = estimate(0);
+    msgJson["estimate_y"] = estimate(1);
+    msgJson["rmse_x"] =  RMSE(0);
+    msgJson["rmse_y"] =  RMSE(1);
+    msgJson["rmse_vx"] = RMSE(2);
+    msgJson["rmse_vy"] = RMSE(3);
     auto msg = "42[\"estimate_marker\"," + msgJson.dump() + "]";
-    std::cout << msg << std::endl;
+    // std::cout << msg << std::endl;
     ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
-
   });
 
   // We don't need this since we're not using HTTP but if it's removed the program doesn't compile :-(
